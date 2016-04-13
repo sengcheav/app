f3b05bd46f6779c89c3d986c23744f12c9303b9e
CSengLeat <cheavleat@Mys-MacBook-Pro.station>
2016-04-13 14:36:31 +1200

    idontknwo

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var app = require('express')();
+//var http = require('http').Server(app);//
+//var io = require('socket.io')(http);
 
+
+var express = require('express')
+  , app = express()
+//  , pg = require('pg').native
+//  , connectionString = process.env.DATABASE_URL
+  //, start = new Date()
+  , port = process.env.PORT ||8080
+  , http = require ('http').Server(app)
+ // , server = require('http').createServer(app)
+  , io = require('socket.io')(http)
+  , client;
+  //, app = express.createServer(express.logger())
+//client = new pg.Client(connectionString);
+//client.connect();
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
 });
