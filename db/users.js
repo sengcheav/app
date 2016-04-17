12b9e89d3abbd23a32b2c9598d083369eff33a7b
CSengLeat <cheavleat@Mys-MacBook-Pro.station>
2016-04-17 19:21:52 +1200

    signup db

--- db/users.js
+++ db/users.js
@@ -1,9 +1,12 @@
 
 
 exports.findByUsername = function(username , cb){
+  process.nextTick(function() {
   client.query ( 'SELECT username FROM account WHERE username =$1' ,[username]);
   query.on('row' , function (res){
     if(!res){ console.log('fail ') ;  cb(null, null );}
     else { console.log('success ') ; cb(null, res) ; }
   });
+
+});
 }
