311c689c6284e1c98aaef0623c3cb8d53ba27be7
CSengLeat <cheavleat@Mys-MacBook-Pro.station>
2016-04-17 14:31:48 +1200

    login

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var  app = express();
+var app = express();
 var pg = require('pg').native ;
 var connectionString = process.env.DATABASE_URL ;
 var start = new Date() ;
@@ -7,24 +7,22 @@ var server = require('http').Server(app);
 var  port = process.env.PORT ||8080;
 var io = require('socket.io')(server);
 var client;
-//, app = express.createServer(express.logger())
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+
 client = new pg.Client(connectionString);
 client.connect();
-
- //, http = require ('http').Server(app)
-  //, io = require('socket.io')(http)
-
-
-//  , client;
+app.use(express.cookieParser()); //just for auth
+app.use(express.bodyParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.get('/1', function(req, res) {
   var date = new Date();
 
   client.query('INSERT INTO visits(date) VALUES($1)', [date]);
-
   query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
   query.on('row', function(result) {
     console.log(result);
-
     if (!result) {
       return res.send('No data found');
     } else {
@@ -53,3 +51,62 @@ io.on('connection', function(socket){
 server.listen(port, function() {
   console.log('Listening on:', port);
 });
+
+//login stuff
+app.get('/login', function ( req,res){
+  res.send(__dirname +'/login.html');
+});
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+  app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+findByUsername(username , cb){
+  client.query ( 'SELECT username FROM account WHERE username =$1' ,[username]);
+  query.on('row' , function (res){
+    if(!res){ console.log('fail ') ;  cb(null, null );}
+    else { console.log('success ') ; cb(null, res) ; }
+  });
+}
--- schema.js
+++ schema.js
@@ -5,5 +5,6 @@ var pg = require('pg').native
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query('CREATE TABLE visits (date date)');
+//query = client.query('CREATE TABLE visits (date date)');
+query = client.query('CREATE TABLE account (username VARCHAR(10) PRIMARY KEY , password VARCAHR(10) )');
 query.on('end', function(result) { client.end(); });
