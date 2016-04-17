38278f1f8cb4474b4d24fa7a9354346ae0dc8773
CSengLeat <cheavleat@Mys-MacBook-Pro.station>
2016-04-17 21:14:45 +1200

    cb

--- index.js
+++ index.js
@@ -141,8 +141,11 @@ findByUsername = function(username , cb){//
     if(result.count <1 ){
       console.log("no user");
       cb(null, null);
-    }else {cb (null, result);}
+    }
+    else {
+      cb (null, result);
+    }
 
   });
 
-});
+}
